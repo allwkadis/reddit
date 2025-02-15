bc02f36eac9e13533f457145935b4cd05bfbdc31
allwkadis <kadisbinance@gmail.com>
2025-02-15 20:05:31 +0500

    add express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello12')
+import express from 'express';
+
+const expresApp = express();
+
+expresApp.get('/posts', (req, res) => {
+  res.send('posts');
+});
+
+expresApp.get('/', (req, res) => {
+  res.send('main');
+});
+
+expresApp.listen(3000, () => {
+  console.info('server start at http://localhost:3000/');
+});
